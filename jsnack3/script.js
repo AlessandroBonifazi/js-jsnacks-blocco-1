f598451a1766c531629ace9c051ca033e6f12405
AlessandroBonifazi <alessandrobonifazi21@gmail.com>
2022-03-10 18:25:25 +0100

    adding js logic

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -1,2 +1,16 @@
 // Debug
 console.log('JS OK!')
+
+// Chiedi un numero di 4 cifre all’utente. 
+// Calcola la somma di tutte le cifre che compongono il numero.
+
+let number = parseInt(prompt("Input a 4 characters number"));
+
+let sum = 0;
+
+while (number) {
+    sum += number % 10;
+    number = Math.floor(number / 10);
+}
+
+console.log(sum);

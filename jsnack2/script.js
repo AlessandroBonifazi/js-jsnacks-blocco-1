f1c1b940cd9653e69e14b9dd3b8901353efa41c4
AlessandroBonifazi <alessandrobonifazi21@gmail.com>
2022-03-10 17:08:48 +0100

    adding odd num logic

--- jsnack2/script.js
+++ jsnack2/script.js
@@ -1,2 +1,17 @@
 // Debug
 console.log('JS OK!')
+
+// Crea un array vuoto. 
+// Chiedi per 6 volte all’utente di inserire un numero, se è dispari inseriscilo nell’array.
+
+const odd = [];
+
+for (let i = 0; i < 6; i++) {
+    let number = parseInt(prompt('Input a number'));
+
+    if (number % 2 === 1) {
+        odd.push(number);
+        console.log('numeri dispari: ' + odd);
+    }
+}
+

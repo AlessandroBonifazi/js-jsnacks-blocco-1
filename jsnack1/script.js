f11c4b08db40c799a9b70391996c0e974cffa901
AlessandroBonifazi <alessandrobonifazi21@gmail.com>
2022-03-10 15:48:49 +0100

    jsnack1 basic logic

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -1,2 +1,28 @@
 // Debug
 console.log('JS OK!')
+
+// In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby,
+// chiedi all’utente il suo nome e comunicagli se può partecipare o no alla festa.
+
+const guestList = ['pippo', 'pluto', 'paperino', 'topolino'];
+
+document.getElementById('check').addEventListener('click',
+    function () {
+
+        const guestName = document.getElementById('guest').value;
+        let nameFound = false;
+
+        for (let i = 0; i < guestList.length && !nameFound; i++) {
+            if (guestName === guestList[i]) {
+                nameFound = true;
+            }
+        }
+
+        const result = document.getElementById('result');
+        if (nameFound = true) {
+            result.innerText = 'Welcome!'
+        } else {
+            result.innerText = 'Your name is not on the list!'
+        }
+    }
+)
